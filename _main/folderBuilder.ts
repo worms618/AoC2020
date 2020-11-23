90aeb7cb7124754cef97538729b4d6a6ff3f7c2f
Remco Vorthoren <remco.boys@gmail.com>
2020-11-23 21:24:59 +0100

    Added input folder to solution folder

--- _main/folderBuilder.ts
+++ _main/folderBuilder.ts
@@ -2,20 +2,26 @@ import {Path, WINDOWS_SEPS} from "https://deno.land/x/path/mod.ts";
 
 const rootFolder = Deno.cwd();
 
+const inputFolderName = 'input';
+
 const startDate = new Date(2020, 11);
 const endDate = new Date(2020, 11, 24);
 let nextDate = startDate;
 
 const createFolderName = (dayOfMonth: number) => `Day-${dayOfMonth}`;
-const createFolderPath = (folderName: string) => new Path(rootFolder + '/' + folderName, WINDOWS_SEPS) 
+const createFolderPath = (folderName: string) => new Path(rootFolder + '/' + folderName, WINDOWS_SEPS);
 
 while (nextDate <= endDate) {
   const dayOfMonth = nextDate.getDate();
   const folderName = createFolderName(dayOfMonth);
   const folderPath = createFolderPath(folderName);
+  const inputFolderPath = new Path(`${folderPath.toString()}/${inputFolderName}`, WINDOWS_SEPS)
 
   if (!folderPath.exists) 
     folderPath.mkDirSync();
+  
+  if (!inputFolderPath.exists)
+    inputFolderPath.mkDirSync();
 
   nextDate.setDate(nextDate.getDate() + 1);
 };
