59ebcd714c105f542175aa30be1b257de54fb158
Remco Vorthoren <remco.boys@gmail.com>
2020-12-02 19:09:12 +0100

    Add solution day 2 part 1 - first try

--- days/Day-2/part1.ts
+++ days/Day-2/part1.ts
@@ -1,5 +1,55 @@
 import { DayScriptExecutor } from "../../src/day/day.types.ts";
 
+type Policy = {
+  min: number,
+  max: number,
+  char: string
+}
+
+type PolicyAndPassword = {
+  policy: Policy,
+  password: string
+}
+
 export const executor: DayScriptExecutor = (input: string): string => {
-  throw new Error('Executor not implemented');
+  const inputLines = input.split('\n').map(line => line.trim()).filter(line => line);
+  const totalValidPassword = inputLines.map(getPolicyAndPasswordOfLine).filter(validatePasswordOverPolicy).length;
+
+  return `Total validate passwords: [${totalValidPassword}]`;
+};
+
+const getPolicyAndPasswordOfLine = (line: string): PolicyAndPassword => {
+  const details = line.split(':').map(value => value.trim());
+
+  return {
+    policy: getPolicy(details[0]),
+    password: details[1]
+  }
+}
+
+const getPolicy = (value: string): Policy => {
+  const details = value.split(' ').map(value => value.trim());
+
+  const constraints = details[0].split('-').map(value => parseInt(value.trim()));
+  const char = details[1];
+
+  return {
+    min: constraints[0],
+    max: constraints[1],
+    char
+  }
+}
+
+const validatePasswordOverPolicy = (policyAndPassword: PolicyAndPassword): boolean => {
+  const { min, max, char } = policyAndPassword.policy;
+  let totalAppearanceOfChar = 0;
+
+  for (const pwChar of policyAndPassword.password) {
+    (pwChar === char) ? totalAppearanceOfChar++ : {};
+
+    if (totalAppearanceOfChar > max)
+      return false;
+  }
+
+  return (totalAppearanceOfChar >= min) && (totalAppearanceOfChar <= max);
 };
