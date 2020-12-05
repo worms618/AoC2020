c33ce8747fea93020f9edb89d65f530d80d7e29d
Remco Vorthoren <remco.boys@gmail.com>
2020-12-05 14:22:01 +0100

    Add solution day 5 part 1

--- days/Day-5/part1.ts
+++ days/Day-5/part1.ts
@@ -1,5 +1,14 @@
 import { DayScriptExecutor } from "../../src/day/day.types.ts";
 
+import { getBoardingPasses, getSeatId } from "./shared.ts";
+
 export const executor: DayScriptExecutor = (input: string): string => {
-  throw new Error('Executor not implemented');
+  const boardingPasses = getBoardingPasses(input);
+
+  const highestSeatID = boardingPasses.map(getSeatId).reduce((maxSeatId, seatId) => {
+    if (seatId > maxSeatId)
+      return seatId;
+    return maxSeatId;
+  }, 0);
+  return `Highest seat ID on a boarding pass: ${highestSeatID.toString()}`;
 };
---
+++ days/Day-5/shared.ts
@@ -0,0 +1,45 @@
+export const getBoardingPasses = (input: string): string[] => {
+  return input
+    .split('\n')
+    .map(line => line.trim())
+    .filter(line => line);
+};
+
+export const getSeatId = (boardingPass: string): number => {
+  const rowPartition = boardingPass.substring(0, 7);
+  const columnPartition = boardingPass.substring(7, boardingPass.length);
+
+  const rowNumber = getDecimalFromBinaryPartition(rowPartition, 'F', 'B');
+  const columnNumber = getDecimalFromBinaryPartition(columnPartition, 'L', 'R');
+
+  return (rowNumber * 8) + columnNumber;
+};
+
+export const getDecimalFromBinaryPartition = (partition: string, lowerHalfChar: string, upperHalfChar: string): number => {
+  const isLowerChar = (char: string) => char === lowerHalfChar;
+  const isUpperChar = (char: string) => char === upperHalfChar;
+  
+  let values: number[] = [];
+
+  for (let f = 0; f < Math.pow(2, partition.length); f++) {
+    values.push(f);
+  }
+  
+  for (let i = 0; i < partition.length; i++) {
+    const partitionChar = partition.charAt(i);
+    
+    if (isLowerChar(partitionChar)) {
+      values = values.slice(0, values.length / 2);
+      continue;
+    }
+    
+    if (isUpperChar(partitionChar)) {
+      values = values.slice(values.length / 2, values.length);
+      continue;
+    }
+    
+    throw new Error(`Unknown char: [${partitionChar}] at index: [${i}] in argument partition: [${partition}]`);
+  };
+
+  return values[0];
+}
