f807835febfb1f96b800d53041f1228f594cc77f
Remco Vorthoren <remco.boys@gmail.com>
2020-12-06 15:01:45 +0100

    Add solution day 6 part 2

--- days/Day-6/part2.ts
+++ days/Day-6/part2.ts
@@ -1,5 +1,34 @@
 import { DayScriptExecutor } from "../../src/day/day.types.ts";
 
+import { createGroupsFromInput } from "./shared.ts";
+
 export const executor: DayScriptExecutor = (input: string): string => {
-  throw new Error('Executor not implemented');
+  const groups = createGroupsFromInput(input);
+
+  const groupsQuestionsAnsweredYes = groups.reduce<Set<string>[]>((allGroups, currentGroup) => {
+    const groupQuestionsAnsweredYes: Set<string> = new Set<string>();
+
+    for (const answer of currentGroup) {
+      Array.from(answer).forEach(char => groupQuestionsAnsweredYes.add(char));
+    }
+
+    for (const answer of currentGroup) {
+      if (groupQuestionsAnsweredYes.size === 0) break;
+
+      Array.from(groupQuestionsAnsweredYes).forEach(answerYes => {
+        if (answer.indexOf(answerYes) < 0) groupQuestionsAnsweredYes.delete(answerYes);
+      });
+    }
+
+    allGroups.push(groupQuestionsAnsweredYes);
+
+    return allGroups;
+  }, []);
+
+  const totalNumberAnswerdYes = groupsQuestionsAnsweredYes.reduce<number>((currTotal, setAnsweredYes) => {
+    currTotal += setAnsweredYes.size;
+    return currTotal;
+  }, 0);
+
+  return `Total answered yes combined: ${totalNumberAnswerdYes}`;
 };
