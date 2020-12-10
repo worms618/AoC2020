249c0d3105e307b226377d1b31e7db5e9b42f016
Remco Vorthoren <remco.boys@gmail.com>
2020-12-10 19:15:25 +0100

    Add solution day 10 part 1

--- days/Day-10/part1.ts
+++ days/Day-10/part1.ts
@@ -1,5 +1,42 @@
 import { DayScriptExecutor } from "../../src/day/day.types.ts";
 
+import { getInputLinesTrimAndFilterEmpty, mapToInteger, sortNumberAsc } from "../shared.ts";
+
 export const executor: DayScriptExecutor = (input: string): string => {
-  throw new Error('Executor not implemented');
+  const joltageRatings: number[] = getInputLinesTrimAndFilterEmpty(input).map(mapToInteger());
+
+  const possibleDiffsAmount = new Map<number, number>([
+    [1,0],
+    [2,0],
+    [3,0]
+  ]);
+  getJoltageDifferences(joltageRatings, possibleDiffsAmount);
+
+  console.log(possibleDiffsAmount);
+  const joltageDifferencesToUse = [1,3];
+  const amountJoltageDifferences = joltageDifferencesToUse.map(diff => possibleDiffsAmount.get(diff)).filter(amount => amount !== undefined);
+  
+  const diffWithAmount: string[] = [];
+  possibleDiffsAmount.forEach((value, key) => diffWithAmount.push(`${key} => ${value}`));
+  const multipleOfAmount = amountJoltageDifferences.reduce((value, amount) => (amount !== undefined) && (value !== undefined) ? value * amount : value);
+
+  return `Joltage differences => Amount joltage differences: [${diffWithAmount.join(', ')}]; Multiple of differences amount: [${joltageDifferencesToUse}] => ${multipleOfAmount}`
+};
+
+const getJoltageDifferences = (joltageRatings: number[], possibleDifferences: Map<number, number>): void => {
+  const incrementJoltageDiff = (joltageDiff: number) => {
+    const currAmount: number | undefined = possibleDifferences.get(joltageDiff);
+    if (currAmount !== undefined) possibleDifferences.set(joltageDiff, currAmount + 1);
+  };
+  
+  const joltages = [0].concat(joltageRatings).sort(sortNumberAsc);
+  
+  for (let i = 1; i < joltages.length; i++) {
+    const joltageDiff = joltages[i] - joltages[i-1];
+    
+    incrementJoltageDiff(joltageDiff);
+  }
+
+  const diffWithBuildInAdapter = 3;  
+  incrementJoltageDiff(diffWithBuildInAdapter);
 };
--- days/shared.ts
+++ days/shared.ts
@@ -1,3 +1,7 @@
 export const getInputLinesTrimAndFilterEmpty = (input: string): string[] => {
   return input.split('\n').map(line => line.trim()).filter(line => line);
 }
+
+export const mapToInteger = (radix?: number | undefined): { (value: string): number } => (value: string): number => parseInt(value, radix);
+
+export const sortNumberAsc = (a: number, b: number) => a - b;
