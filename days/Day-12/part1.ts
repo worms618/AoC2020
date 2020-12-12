0fa4b928d9fb9be842ceb921e35ccc632b937442
Remco Vorthoren <remco.boys@gmail.com>
2020-12-12 13:56:31 +0100

    Made day 12 part 1 abstract to use in part 2

--- days/Day-12/part1.ts
+++ days/Day-12/part1.ts
@@ -3,11 +3,18 @@ import { DayScriptExecutor } from "../../src/day/day.types.ts";
 import {
   FacingPosition,
   getInstructions,
-  NavigationPosition,
-  applyInstructionOverPosition,
+  BaseNavigationPosition,
   NavigationInstruction,
-  calculateManhattanDistance
+  calculateManhattanDistance,
+  createNavigationInstructionActions,
+  applyInstructionOverPosition
 } from "./shared.ts";
+
+type NavigationInstructionAction = (pos: NavigationPosition, value: number) => NavigationPosition;
+
+export type NavigationPosition = BaseNavigationPosition & {
+  Facing: FacingPosition
+};
 
 export const executor: DayScriptExecutor = (input: string): string => {
   const instructions = getInstructions(input);
@@ -17,11 +24,100 @@ export const executor: DayScriptExecutor = (input: string): string => {
     Facing: FacingPosition.East
   };
 
+  const instructionActions = createNavigationInstructionActions([
+    ActionNorth,
+    ActionSouth,
+    ActionEast,
+    ActionWest,
+    ActionRight,
+    ActionLeft,
+    ActionForward
+  ]);
+
   const finalPos = instructions.reduce((curPos: NavigationPosition, instruction: NavigationInstruction) => {
-    return applyInstructionOverPosition(curPos, instruction)
+    return applyInstructionOverPosition(curPos, instruction, instructionActions)
   }, startPos);
 
   const manhattanDisFinalPos = calculateManhattanDistance(finalPos);
 
   return `Manhattan distance between final position and start position: ${manhattanDisFinalPos}`;
+};
+
+const ActionNorth = (pos: NavigationPosition, value: number): NavigationPosition => {
+  return {
+    Horizontal: pos.Horizontal,
+    Vertical: pos.Vertical + value,
+    Facing: pos.Facing
+  }
+};
+
+const ActionSouth = (pos: NavigationPosition, value: number): NavigationPosition => {
+  return {
+    Horizontal: pos.Horizontal,
+    Vertical: pos.Vertical - value,
+    Facing: pos.Facing
+  }
+};
+
+const ActionEast = (pos: NavigationPosition, value: number): NavigationPosition => {
+  return {
+    Horizontal: pos.Horizontal + value,
+    Vertical: pos.Vertical,
+    Facing: pos.Facing
+  }
+};
+
+const ActionWest = (pos: NavigationPosition, value: number): NavigationPosition => {
+  return {
+    Horizontal: pos.Horizontal - value,
+    Vertical: pos.Vertical,
+    Facing: pos.Facing
+  }
+};
+
+const ActionRight = (pos: NavigationPosition, value: number): NavigationPosition => {
+  let newFacing = pos.Facing;
+  if ((value !== 360) && (value !== 0)) {
+    const facingInOrder = [
+      FacingPosition.North,
+      FacingPosition.East,
+      FacingPosition.South,
+      FacingPosition.West
+    ];
+
+    if (value < 0) facingInOrder.reverse();
+
+    const indexIncrement = (Math.abs(value) / 90);
+    const indexCurrFacing = facingInOrder.indexOf(pos.Facing);
+    let indexNewFacing = indexCurrFacing + indexIncrement;
+    indexNewFacing = indexNewFacing % facingInOrder.length;
+
+    newFacing = facingInOrder[indexNewFacing];
+  }
+
+  return {
+    Horizontal: pos.Horizontal,
+    Vertical: pos.Vertical,
+    Facing: newFacing
+  }
+};
+
+const ActionLeft = (pos: NavigationPosition, value: number): NavigationPosition => {
+  return ActionRight(pos, -value);
+};
+
+const ActionForward = (pos: NavigationPosition, value: number): NavigationPosition => {
+  switch (pos.Facing) {
+    case FacingPosition.North:
+      return ActionNorth(pos, value);
+    case FacingPosition.East:
+      return ActionEast(pos, value);
+    case FacingPosition.South:
+      return ActionSouth(pos, value);
+    case FacingPosition.West:
+      return ActionWest(pos, value);
+
+    default:
+      throw new Error(`No action possible while facing: ${pos.Facing}`)
+  }
 };
--- days/Day-12/shared.ts
+++ days/Day-12/shared.ts
@@ -9,7 +9,7 @@ export enum FacingPosition {
   North, East, South, West
 };
 
-export type NavigationPosition = {
+export type BaseNavigationPosition = {
   /**
    * East or west position
    * East by Horizontal >= 0
@@ -23,11 +23,11 @@ export type NavigationPosition = {
    * South by Vertical < 0
    */
   Vertical: number;
-
-  Facing: FacingPosition
 };
 
-type NavigationInstructionAction = (pos: NavigationPosition, value: number) => NavigationPosition;
+export type BaseNavigationInstructionAction<P> = {
+  (pos: P, value: number): P
+}
 
 export const getInstructions = (input: string): NavigationInstruction[] => {
   return getInputLinesTrimAndFilterEmpty(input)
@@ -47,104 +47,26 @@ const getInstruction = (line: string): NavigationInstruction => {
   throw new Error(`Could not instruction details for line: ${line}`);
 };
 
-export const calculateManhattanDistance = (pos: NavigationPosition): number => {
+export const calculateManhattanDistance = (pos: BaseNavigationPosition): number => {
   return Math.abs(pos.Vertical) + Math.abs(pos.Horizontal);
 }
 
-export const applyInstructionOverPosition = (pos: NavigationPosition, instruction: NavigationInstruction): NavigationPosition => {
-  const action = InstructionActions.get(instruction.Action);
+export function createNavigationInstructionActions<T>(actions: T[]): Map<string, T> {
+  return new Map<string, T>(
+    [
+      ['N', actions[0]],
+      ['S', actions[1]],
+      ['E', actions[2]],
+      ['W', actions[3]],
+      ['R', actions[4]],
+      ['L', actions[5]],
+      ['F', actions[6]]
+    ]
+  );
+}
+
+export function applyInstructionOverPosition<P, A extends BaseNavigationInstructionAction<P>>(pos: P, instruction: NavigationInstruction, instructionActions: Map<string, A>): P {
+  const action = instructionActions.get(instruction.Action);
   if (action) return action(pos, instruction.Value);
   throw new Error(`No action for [${instruction.Action}]`);
 };
-
-
-const ActionNorth = (pos: NavigationPosition, value: number): NavigationPosition => {
-  return {
-    Horizontal: pos.Horizontal,
-    Vertical: pos.Vertical + value,
-    Facing: pos.Facing
-  }
-};
-
-const ActionSouth = (pos: NavigationPosition, value: number): NavigationPosition => {
-  return {
-    Horizontal: pos.Horizontal,
-    Vertical: pos.Vertical - value,
-    Facing: pos.Facing
-  }
-};
-
-const ActionEast = (pos: NavigationPosition, value: number): NavigationPosition => {
-  return {
-    Horizontal: pos.Horizontal + value,
-    Vertical: pos.Vertical,
-    Facing: pos.Facing
-  }
-};
-
-const ActionWest = (pos: NavigationPosition, value: number): NavigationPosition => {
-  return {
-    Horizontal: pos.Horizontal - value,
-    Vertical: pos.Vertical,
-    Facing: pos.Facing
-  }
-};
-
-const ActionRight = (pos: NavigationPosition, value: number): NavigationPosition => {
-  let newFacing = pos.Facing;
-  if ((value !== 360) && (value !== 0)) {
-    const facingInOrder = [
-      FacingPosition.North,
-      FacingPosition.East,
-      FacingPosition.South,
-      FacingPosition.West
-    ];
-
-    if (value < 0) facingInOrder.reverse();
-
-    const indexIncrement = (Math.abs(value) / 90);
-    const indexCurrFacing = facingInOrder.indexOf(pos.Facing);
-    let indexNewFacing = indexCurrFacing + indexIncrement;
-    indexNewFacing = indexNewFacing % facingInOrder.length;
-
-    newFacing = facingInOrder[indexNewFacing];
-  }
-
-  return {
-    Horizontal: pos.Horizontal,
-    Vertical: pos.Vertical,
-    Facing: newFacing
-  }
-};
-
-const ActionLeft = (pos: NavigationPosition, value: number): NavigationPosition => {
-  return ActionRight(pos, -value);
-};
-
-const ActionForward = (pos: NavigationPosition, value: number): NavigationPosition => {
-  switch (pos.Facing) {
-    case FacingPosition.North:
-      return ActionNorth(pos, value);
-    case FacingPosition.East:
-      return ActionEast(pos, value);
-    case FacingPosition.South:
-      return ActionSouth(pos, value);
-    case FacingPosition.West:
-      return ActionWest(pos, value);
-
-    default:
-      throw new Error(`No action possible while facing: ${pos.Facing}`)
-  }
-};
-
-const InstructionActions = new Map<string, NavigationInstructionAction>(
-  [
-    ['N', ActionNorth],
-    ['S', ActionSouth],
-    ['E', ActionEast],
-    ['W', ActionWest],
-    ['R', ActionRight],
-    ['L', ActionLeft],
-    ['F', ActionForward],
-  ]
-);
