106963b161ff9fa14d18ff6b67102ad1f9b9611a
Remco Vorthoren <remco.boys@gmail.com>
2020-12-13 14:24:33 +0100

    Add solution for day 13 part 1

--- days/Day-13/part1.ts
+++ days/Day-13/part1.ts
@@ -1,5 +1,34 @@
 import { DayScriptExecutor } from "../../src/day/day.types.ts";
 
+import { getNotes } from "./shared.ts";
+
 export const executor: DayScriptExecutor = (input: string): string => {
-  throw new Error('Executor not implemented');
+  const notes = getNotes(input);
+
+  const { EarliestTime, PossibleBuses } = notes;
+  
+  let earliestTimeToLeave = Number.MAX_SAFE_INTEGER;
+  let busToTake = -1;
+
+  for (let i = 0; i < PossibleBuses.length; i++) {
+    const bus = PossibleBuses[i];
+
+    if ((EarliestTime % bus) === 0) {
+      earliestTimeToLeave = EarliestTime; 
+      busToTake = bus;
+      break;
+    } else {
+      const nextTimeBusLeaves = Math.ceil(EarliestTime / bus) * bus;
+
+      if (nextTimeBusLeaves < earliestTimeToLeave) {
+        earliestTimeToLeave = nextTimeBusLeaves;
+        busToTake = bus;
+      }
+    }
+
+  }
+
+  const timeToWait = earliestTimeToLeave - EarliestTime;
+
+  return `${timeToWait * busToTake}`;
 };
