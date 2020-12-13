d63f8ea7dc7303210bc367752943af731f627564
Remco Vorthoren <remco.boys@gmail.com>
2020-12-13 16:46:34 +0100

    Add bruteforce solution day 13 part 2

--- days/Day-13/part1.ts
+++ days/Day-13/part1.ts
@@ -1,6 +1,6 @@
 import { DayScriptExecutor } from "../../src/day/day.types.ts";
 
-import { getNotes } from "./shared.ts";
+import { getInputLinesTrimAndFilterEmpty, mapToInteger } from "../shared.ts";
 
 export const executor: DayScriptExecutor = (input: string): string => {
   const notes = getNotes(input);
@@ -31,4 +31,18 @@ export const executor: DayScriptExecutor = (input: string): string => {
   const timeToWait = earliestTimeToLeave - EarliestTime;
 
   return `${timeToWait * busToTake}`;
+};
+
+type EarliestTimeAndPossibleBuses = {
+  EarliestTime: number;
+  PossibleBuses: number[];
+}
+
+const getNotes = (input: string): EarliestTimeAndPossibleBuses => {
+  const lines = getInputLinesTrimAndFilterEmpty(input);
+
+  return {
+    EarliestTime: parseInt(lines[0], 10),
+    PossibleBuses: lines[1].split(',').map(mapToInteger(10)).filter(bus => Number.isInteger(bus))
+  }
 };
--- days/Day-13/part2.ts
+++ days/Day-13/part2.ts
@@ -1,5 +1,38 @@
 import { DayScriptExecutor } from "../../src/day/day.types.ts";
 
+import { mapToInteger } from "../shared.ts";
+
 export const executor: DayScriptExecutor = (input: string): string => {
-  throw new Error('Executor not implemented');
+  const buses = input.split(',').map(mapToInteger(10)).map(value => Number.isInteger(value) ? value : undefined);
+
+  const earliestTimestamp = bruteForce(buses);
+
+  return `${earliestTimestamp}`;
+};
+
+const isMultipleOf = (value: number, base: number) => (value % base) === 0;
+
+const bruteForce = (buses: (number | undefined)[]): number => {
+  let earliestTimestamp = 100000000000000;
+
+  while (earliestTimestamp < Number.MAX_SAFE_INTEGER) {
+    let isCorrectSequence = true;
+
+    for (let i = 0; i < buses.length; i++) {
+      const bus = buses[i];
+
+      if (bus === undefined) continue;
+
+      if (!isMultipleOf(earliestTimestamp + i, bus)) {
+        isCorrectSequence = false;
+        break;
+      }
+    }
+
+    if (isCorrectSequence) return earliestTimestamp;
+
+    earliestTimestamp++;
+  };
+
+  return -1;
 };
