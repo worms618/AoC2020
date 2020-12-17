b364a41446f7eb76cddc283bed30eebedf548419
Remco Vorthoren <remco.boys@gmail.com>
2020-12-17 20:30:44 +0100

    Add base for the solution day 17 part 1

--- days/Day-17/part1.ts
+++ days/Day-17/part1.ts
@@ -1,5 +1,16 @@
 import { DayScriptExecutor } from "../../src/day/day.types.ts";
 
+import { getInitialPocketDimension, printPocketDimension, runCycle } from "./shared.ts";
+
 export const executor: DayScriptExecutor = (input: string): string => {
+  const pdim = getInitialPocketDimension(input);
+
+  printPocketDimension(pdim);
+
+  const afterOneRun = runCycle(pdim);
+
+  console.log('--- After one cycle ---');
+  printPocketDimension(afterOneRun);
+
   throw new Error('Executor not implemented');
 };
