2010a65cd233c7df9928f73162cb521fb0cfe405
Remco Vorthoren <remco.boys@gmail.com>
2020-12-21 12:03:05 +0100

    Add solution day 21 part 1

--- days/Day-21/part1.ts
+++ days/Day-21/part1.ts
@@ -1,5 +1,61 @@
 import { DayScriptExecutor } from "../../src/day/day.types.ts";
 
+import { getInputLinesTrimAndFilterEmpty } from '../shared.ts';
+
 export const executor: DayScriptExecutor = (input: string): string => {
-  throw new Error('Executor not implemented');
+  const lines = getInputLinesTrimAndFilterEmpty(input);
+
+  const allIngredients = new Set<string>();
+  const allergensInIngredients: Map<string, string[]> = new Map<string, string[]>();
+
+  const ingredientsPerLine: string[][] = [];
+
+  const getIngredientsWithAllergensRegExp = /(.*)\(contains(.*)\)/;
+  lines.forEach(line => {
+    const regRes = getIngredientsWithAllergensRegExp.exec(line);
+
+    if (regRes && (regRes.length > 2)) {
+      const ingredients = regRes[1].trim().split(' ').map(i => i.trim());
+      const allergens = regRes[2].trim().split(',').map(a => a.trim());
+
+      ingredientsPerLine.push(ingredients);
+
+      ingredients.forEach(ingredient => allIngredients.add(ingredient));
+      allergens.forEach(allergen => {
+        let knownIngredients = allergensInIngredients.get(allergen);
+
+        if (knownIngredients === undefined)
+          knownIngredients = Array.from(ingredients);
+        else {
+          const newKnowIngredients = [];
+
+          //Intersection? Only add the ingredient that is already in the list
+          for (const ingredient of ingredients) {
+            if ((knownIngredients.indexOf(ingredient) >= 0)) {
+              newKnowIngredients.push(ingredient);
+            }
+          }
+
+          knownIngredients = newKnowIngredients;
+        }
+
+        allergensInIngredients.set(allergen, knownIngredients);
+      });
+    }
+  });
+
+  console.log(allergensInIngredients)
+
+  const ingredientsWithAllergen: Set<string> = new Set<string>();
+  for (const ingredients of allergensInIngredients.values()) {
+    ingredients.forEach(ingredient => ingredientsWithAllergen.add(ingredient));
+  }
+
+  console.log(ingredientsWithAllergen)
+
+  let total = ingredientsPerLine.reduce((total, ingredients) => {
+    return total + (ingredients.filter(ingredient => !ingredientsWithAllergen.has(ingredient)).length);
+  }, 0);
+
+  return `${total}`;
 };
